4a20329e4ea54260c9734a766f02b17b07396fff
shaiqkar <shaiqkar@gmail.com>
2021-03-29 16:19:11 +0530

    Apollo Server

---
+++ server/schema.js
@@ -0,0 +1,11 @@
+import { makeExecutableSchema } from 'graphql-tools';
+
+import { typeDefs } from './graphql/typedefs';
+import { resolvers } from './graphql/resolvers';
+
+const schema = makeExecutableSchema({
+	typeDefs,
+	resolvers,
+});
+
+export default schema;
