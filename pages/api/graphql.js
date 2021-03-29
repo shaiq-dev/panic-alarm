5b58d3f5f5bf494e6ffabf10d39fe2b025a07a25
shaiqkar <shaiqkar@gmail.com>
2021-03-29 16:17:47 +0530

    Graphql API EndPoint

---
+++ pages/api/graphql.js
@@ -0,0 +1,36 @@
+import { ApolloServer } from 'apollo-server-micro';
+import { MongoClient } from 'mongodb';
+
+import schema from '@/server/schema';
+
+require('dotenv').config();
+
+let db;
+
+const apolloServer = new ApolloServer({
+	schema,
+	context: async () => {
+		if (!db) {
+			try {
+				const dbClient = new MongoClient(process.env.MONGO_DB_CON, {
+					useNewUrlParser: true,
+					useUnifiedTopology: true,
+				});
+
+				if (!dbClient.isConnected()) await dbClient.connect();
+				db = dbClient.db('panicAlarmDevDb');
+			} catch (e) {
+				console.log('[ERROR] Connecting With DB Client', e);
+			}
+		}
+		return { db };
+	},
+});
+
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+};
+
+export default apolloServer.createHandler({ path: '/api/graphql' });
