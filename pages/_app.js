cb11fd47c98537bcb94c2e0a4a8f9111c02f0142
shaiqkar <shaiqkar@gmail.com>
2021-03-29 14:15:41 +0530

    CC#1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import '../styles/styles.scss';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default App;
