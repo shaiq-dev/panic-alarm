df90bb959b26a754aa57c9d9b35810bb3912f6bd
shaiqkar <shaiqkar@gmail.com>
2021-04-06 13:43:06 +0530

    Wrapped with AuthContext Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,41 @@
+import App from 'next/app';
 import '../styles/styles.scss';
-import { withApollo } from '@/server/ApolloProvider';
 
-function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+import { withApollo } from '@/server/ApolloProvider';
+import { AuthProvider } from 'context/auth';
+import checkAuth from '@/server/Utils/checkAuth';
+
+class PanicAlarmApp extends App {
+	render() {
+		const { Component, pageProps, user, authenticated } = this.props;
+		return (
+			<AuthProvider user={user} authenticated={authenticated}>
+				<Component {...pageProps} />
+			</AuthProvider>
+		);
+	}
 }
 
-export default withApollo(App);
+PanicAlarmApp.getInitialProps = async (appCtx) => {
+	let authenticated = false;
+	let user = null;
+
+	const request = appCtx.ctx.req;
+	if (request) {
+		const authToken = request.cookies.authToken;
+		if (authToken) {
+			try {
+				user = checkAuth(authToken);
+				authenticated = true;
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	}
+
+	const appProps = await App.getInitialProps(appCtx);
+
+	return { ...appProps, authenticated, user };
+};
+
+export default withApollo(PanicAlarmApp);
