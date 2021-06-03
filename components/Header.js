45e4f6685b6a453faa6b16a5d9feb72e1e927bb5
shaiqkar <shaiqkar@gmail.com>
2021-06-03 15:55:55 +0530

    Partially Working Logout Function

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,23 @@
+import { gql, useMutation } from '@apollo/client';
 import { ExportOutlined } from '@ant-design/icons';
 import store from 'store';
 
 import Logo from 'assets/logoWhite.svg';
+import { Router } from 'next/router';
 
 export default function Header({ name }) {
+	const [logout, { loading }] = useMutation(LOGOUT_USER, {
+		update(_, { data }) {
+			Router.push('/auth/login?logout=true');
+		},
+		variables: {
+			lKey: 'pa-lout',
+		},
+	});
+	const handleLogout = () => {
+		logout();
+	};
+
 	return (
 		<header>
 			<Logo className="logo" />
@@ -15,10 +29,16 @@ export default function Header({ name }) {
 					Hello , {name}
 				</span>
 				{/* TODO: Implement Logout */}
-				<a href="#" className="user-logout">
+				<a href="#" className="user-logout" onClick={handleLogout}>
 					<ExportOutlined />
 				</a>
 			</div>
 		</header>
 	);
 }
+
+const LOGOUT_USER = gql`
+	mutation logoutUser($lKey: String) {
+		logoutUser(lkey: $lKey)
+	}
+`;
