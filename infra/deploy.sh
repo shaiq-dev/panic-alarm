0b59a9aa7355803301a7d83977fe47f293b0615b
shaiq-dev <shaiqkar@gmail.com>
2024-06-27 00:13:42 +0530

    rds deployment

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 
 BASE_DIR=$GITHUB_WORKSPACE
 
+# Deploy VPC
 ${BASE_DIR}/infra/scripts/deploy-cfn.sh --template ${BASE_DIR}/infra/stacks/vpc/stack.yml --stack-name vpc
+
+# Deploy RDS
+${BASE_DIR}/infra/scripts/deploy-cfn.sh --template ${BASE_DIR}/infra/stacks/rds/stack.yml --stack-name rds
