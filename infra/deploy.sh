da1f8ac316c52b42cc1179426e6749f611e9c7de
Shaiq Kar <53471824+shaiq-dev@users.noreply.github.com>
2024-06-26 23:58:44 +0530

    use `$GITHUB_WORKSPACE` to reference workspace root (#305)

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-BASE_DIR=$(cd $(dirname ${0})/../..; pwd)
+BASE_DIR=$GITHUB_WORKSPACE
 
 ${BASE_DIR}/infra/scripts/deploy-cfn.sh --template ${BASE_DIR}/infra/stacks/vpc/stack.yml --stack-name vpc
